b78d78954fcf14468d49b0b0003579f92a74b90c
AndyRosenberg <andynaterosenberg@gmail.com>
2020-09-05 15:19:35 -0700

    Take away class variable

--- config/initializers/cache.rb
+++ config/initializers/cache.rb
@@ -1,12 +1,9 @@
 class RodaCache
   def self.set(*args)
-    source.redis { |redis| redis.set(*args) }
+    SideKiq.redis { |redis| redis.set(*args) }
   end
 
   def self.get(key)
-    source.redis { |redis| redis.get(key) }
+    Sidekiq.redis { |redis| redis.get(key) }
   end
-
-  private
-  cattr_reader :source, instance_reader: false, default: Sidekiq
 end
