1ff56de8843cee3557691bf37e3dd6ceb24ae9ee
AndyRosenberg <andynaterosenberg@gmail.com>
2020-09-11 13:09:44 -0700

    by type endpoint

--- controllers/photos_controller.rb
+++ controllers/photos_controller.rb
@@ -1,15 +1,27 @@
 class PhotosController < Roda
   route do |r|
+    r.on "by_type", String, Integer do |type, id|
+      self.photo = Photo.find_by(photoable_type: type.capitalize, photoable_id: id)
+      render_photo_body?
+    end
+
     r.on Integer do |id|
       r.is do
         r.get do
-          photo = Photo.find_by_id(id)
-          return render_unprocessable unless photo
-          photo_body = photo.get_from_s3
-          return render_unprocessable unless photo_body
-          render_json({ "photo" => photo_body }.to_json)
+          self.photo = Photo.find_by_id(id)
+          render_photo_body?
         end
       end
     end
+  end
+
+  private
+  attr_accessor :photo
+
+  def render_photo_body?
+    return render_unprocessable unless photo
+    photo_body = photo.get_from_s3
+    return render_unprocessable unless photo_body
+    render_json({ "photo" => photo_body }.to_json)
   end
 end
